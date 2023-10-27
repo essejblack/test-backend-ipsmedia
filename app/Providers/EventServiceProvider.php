1387605464cbde9afadd7a351d6c4ab773367e06
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-28 01:05:54 +0330

    lesson user observer called

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,9 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+        'eloquent.sync: App\User' => [
+            'App\Observers\LessonUserObserver@sync',
+        ],
     ];
 
     /**
