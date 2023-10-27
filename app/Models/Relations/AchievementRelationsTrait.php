ff81b12f4c0f3c74959cf24cb4d08fc8a17aea17
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-27 20:05:31 +0330

    achievement controller and model added

---
+++ app/Models/Relations/AchievementRelationsTrait.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Models\Relations;
+
+use App\Models\Comment;
+use App\Models\Lesson;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+trait AchievementRelationsTrait
+{
+
+}
