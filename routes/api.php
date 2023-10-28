4fdf29c9f91248e8c04e5e0451f088f144f15c13
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-28 17:50:16 +0330

    needed endpoints added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Controllers\API\AchievementController;
+use App\Http\Controllers\API\CommentController;
+use App\Http\Controllers\API\LessonController;
+use App\Http\Controllers\API\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +20,25 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+
+Route::middleware('throttle:2000,1')->group(function () {
+    Route::prefix('users')->group(function () {
+        Route::get('', [UserController::class, 'index']);
+        Route::get('/{user}/achievements', [UserController::class, 'achievements']);
+    });
+
+    Route::prefix('lessons')->group(function () {
+        Route::post('/{lesson}/{user}', [LessonController::class, 'seen']);
+    });
+
+    Route::prefix('achievements')->group(function () {
+        Route::get('', [AchievementController::class, 'index']);
+    });
+
+    Route::prefix('lessons')->group(function () {
+        Route::get('/{lesson}/comments', [LessonController::class, 'index']);
+        Route::post('/{lesson}/comments', [CommentController::class, 'store']);
+    });
 });
