de1f668e8f3ca142437e11e73b72b489f0535eea
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-28 17:52:45 +0330

    user achievement resource is now using achievement methods

--- app/Http/Resources/UserAchievementResource.php
+++ app/Http/Resources/UserAchievementResource.php
@@ -3,22 +3,21 @@
 namespace App\Http\Resources;
 
 use App\Models\Achievement;
+use App\Models\Badge;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class UserAchievementResource extends JsonResource
 {
     public function toArray($request): array
     {
-        $achievement = $this->achievements()->where('user_id', $this->id)->last();
-
         return [
-            'unlocked_achievements' => AchievementResource::collection($achievement),
-            'next_available_achievements' => AchievementResource::collection(Achievement::query()->whereDoesntHave('users', function ($query) {
-                $query->where('user_id', $this->id);
-            })->get()),
-            'current_badge' => '',
-            'next_badge' => '',
-            'remaing_to_unlock_next_badge' => 0,
+            'unlocked_achievements' => AchievementResource::make(Achievement::unlockedAchievement($this->resource)),
+            'next_available_achievements' => AchievementResource::collection(Achievement::getNextAchievements($this->resource)),
+            'current_badge' => $this->badge->name,
+            'next_badge' => Badge::where('id', '>', $this->badge->id)
+                ->orderBy('id')
+                ->first()?->name,
+            'remaing_to_unlock_next_badge' => Badge::where('id', '>', $this->badge->id)->orderBy('id')->count(),
         ];
     }
 }
