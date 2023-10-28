45fcdf15e9fcf497101f5be3c628ed3550703ab1
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-28 17:50:58 +0330

    achievement methods added to get unlocked and locked by user

--- app/Models/Achievement.php
+++ app/Models/Achievement.php
@@ -10,4 +10,34 @@ class Achievement extends Model
 {
     use HasFactory;
     use AchievementRelationsTrait;
+
+    public static function unlockedAchievement(User $user)
+    {
+        return self::unlockedAchievements($user)->last();
+    }
+
+    public static function getNextAchievement(User $user)
+    {
+        return self::getNextAchievements($user)->first();
+    }
+
+    public static function unlockedAchievements(User $user)
+    {
+        return $user->achievements()->where('user_id', $user->id)->get();
+    }
+
+    public static function getNextAchievements(User $user)
+    {
+        $unlockedAchievement = ($unlocked = static::unlockedAchievement($user)) ? $unlocked : null;
+
+        $query = static::query()->whereDoesntHave('users', function ($query) use ($user) {
+            $query->where('user_id', $user->id);
+        });
+
+        if ($unlockedAchievement){
+            $query->where('id', '>', $unlockedAchievement->id);
+        }
+
+        return $query->get();
+    }
 }
