8414642041f5579fb339cd309ae879b84f9060a8
Ehsan Jahanara <ehsan.jnra@gmail.com>
2023-10-28 17:52:12 +0330

    lesson seen by user fixed

--- app/Http/Controllers/API/LessonController.php
+++ app/Http/Controllers/API/LessonController.php
@@ -2,13 +2,29 @@
 
 namespace App\Http\Controllers\API;
 
+use App\Http\Controllers\ApiController;
 use App\Http\Controllers\Controller;
+use App\Models\Badge;
 use App\Models\Lesson;
+use App\Models\LessonUser;
+use App\Models\User;
 
-class LessonController extends Controller
+class LessonController extends ApiController
 {
     public function index()
     {
         Lesson::all();
+    }
+
+    public function seen(Lesson $lesson, User $user)
+    {
+        LessonUser::query()->createOrFirst([
+            'user_id' => $user->id,
+            'lesson_id' => $lesson->id,
+        ], [
+            'watched' => true,
+        ]);
+
+        return $this->response('Success');
     }
 }
